c5c3a265408a273d8fda63cb9d992c113532d494
r-suzuki0628 <lludou29@gmail.com>
2024-05-11 21:21:36 +0900

    feat: 定数ファイルを追加

---
+++ scripts/utils/constant.py
@@ -0,0 +1,13 @@
+# -*- coding: utf_8 -*-
+"""ログ用の定数
+* ログの定数を定義していいる
+"""
+
+LOG_START = "処理を開始します。"
+LOG_END = "処理を終了します。"
+LOG_ERROR_END = "処理を異常終了します。"
+
+RESULTOK = True
+RESULTERR = False
+
+LOG_ERR_MSG01 = "環境変数ファイルが存在しません"
